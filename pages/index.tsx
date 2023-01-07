2208d73999875e1630007ebc9b00c33af47e82cc
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-01-07 20:37:26 +0100

    Hero section included in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,39 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import Header from '../components/Header'
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Moussaab&#39;s Portfolio</title>       
-      </Head>
+    <div className="bg-slate-900">
+      <div
+        className="h-screen snap-y snap-mandatory 
+    overflow-scroll z-0 "
+      >
+        <Head>
+          <title>Moussaab&#39;s Portfolio</title>
+        </Head>
 
-      { /* HEADER */}
-      <Header />
+        {/* HEADER */}
+        <Header />
 
-      { /* HERO */}
-      
-      { /* About */}
-      
-      { /* Experience */}
-      
-      { /* Skills */}
+        {/* HERO */}
+        <section id="hero" className="snap-center">
+          <Hero />
+        </section>
 
-      { /* Projects */}
-      
-      { /* Contact */}
+        {/* About */}
 
+        {/* Experience */}
+
+        {/* Skills */}
+
+        {/* Projects */}
+
+        {/* Contact */}
+      </div>
     </div>
-    
-  )
+  );
 }
-
