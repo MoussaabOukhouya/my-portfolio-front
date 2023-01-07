42ac2f70b8390ce46f49010bd13466780e7c81aa
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-01-07 20:35:52 +0100

    Creation of the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 
@@ -5,10 +6,42 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>
-        <div>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto md'>
+        <motion.div
+        initial = {{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate = {{
+          x: 0,
+          opacity: 1,
+          scale: 1,         
+        }}
+        transition = {{
+          duration: 1
+        }}>
             <SocialIcon url='https://www.linkedin.com/in/oukhouyamoussaab/'/>
-        </div>
+        </motion.div>
+
+        <motion.div className=' cursor-pointer'
+        initial = {{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate = {{
+          x: 0,
+          opacity: 1,
+          scale: 1,         
+        }}
+        transition = {{
+          duration: 1
+        }}>
+          <p>
+            Get in touch
+          </p>
+        </motion.div>
     </header>
   );
 }
