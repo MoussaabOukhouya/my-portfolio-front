3fd126852ace47a071b06b4cd11e2a47fe1a4011
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-01-11 22:31:16 +0100

    Hero Z-index defined in order to show menu in front

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,18 @@ export default function Home() {
     <div className="bg-slate-900">
       <div
         className="h-screen snap-y snap-mandatory 
-    overflow-scroll z-0 "
+    overflow-scroll z-0"
       >
         <Head>
           <title>Moussaab&#39;s Portfolio</title>
         </Head>
 
         {/* HEADER */}
+      
+        
         <Header />
-
         {/* HERO */}
-        <section id="hero" className="snap-center">
+        <section id="hero" className="snap-center z-1">
           <Hero />
         </section>
 
