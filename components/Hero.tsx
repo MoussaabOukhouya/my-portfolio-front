abe6a7e7fdae2b7f410df0ed3957d4b412e3d1e1
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-01-16 02:13:55 +0100

    made changes on Z-index and position of items

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
 import HeroBackground from "./HeroBackground";
 import Image from "next/image";
-import me from "./../assets/Images/Me.png"
-import logo from "./../assets/Images/MyLogo.png"
+import me from "./../assets/Images/Me.png";
+import logo from "./../assets/Images/MyLogo.png";
 
 type Props = {};
 
@@ -19,22 +19,26 @@ export default function Hero({}: Props) {
     delaySpeed: 2000,
   });
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center ">
-      <HeroBackground />
-      <Image
-        className="relative rounded-full h-32 w-32 mx-auto object-cover"
-        src={me}
-        alt="Profile Image"
-      />
-      <div>
-        <h2 className="bg-clip-text lg:text-3xl flex justify-center uppercase  pb-2 tracking-[8px] font-bold font-Spline
-        animate-text bg-gradient-to-r from-purple-500 via-yellow-300 to-red-500 text-xl  text-transparent">
-          Software Engineer
-        </h2>
-        <h1 className="text-xl lg:text-3xl  px-10 font-Acme ">
-          <span className="text-gray-200 mr-3 bg-clip-text  ">{text}</span>
-          <Cursor cursorColor="#F7AB0A" />
-        </h1>
+    <div className="relative h-screen flex flex-col space-y-8 justify-center items-center text-center overflow-hidden z-0">
+      <HeroBackground backgroundType="Hero" />
+      <div className="absolute flex flex-col justify-center   ">
+        <Image
+          className=" rounded-full h-32 w-32 mx-auto object-cover "
+          src={me}
+          alt="Profile Image"
+        />
+        <div>
+          <h2
+            className="bg-clip-text lg:text-3xl flex justify-center uppercase  pb-2 tracking-[8px] font-bold font-Spline 
+        animate-text bg-gradient-to-r from-purple-500 via-yellow-300 to-red-500 text-xl  text-transparent"
+          >
+            Software Engineer
+          </h2>
+          <h1 className="text-xl lg:text-3xl  px-10 font-Acme  ">
+            <span className="text-gray-200 mr-3 bg-clip-text  ">{text}</span>
+            <Cursor cursorColor="#F7AB0A" />
+          </h1>
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +22,15 @@ export default function Home() {
         
         <Header />
         {/* HERO */}
-        <section id="hero" className="snap-center z-1">
+        <section id="hero" className="snap-start ">
           <Hero />
         </section>
 
         {/* About */}
-
+        <section id="About" className="snap-center ">
+          <About />
+        </section>
+          
         {/* Experience */}
 
         {/* Skills */}
