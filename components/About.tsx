444b1b6e63bbc907f634ad9114dd94b97d641bae
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-01-19 21:36:55 +0100

    z-index not needed anymore on this compo

--- components/About.tsx
+++ components/About.tsx
@@ -5,8 +5,8 @@ type Props = {}
 
 export default function About({}: Props) {
   return (
-    <div className='relative h-screen flex flex-col space-y-8 justify-center items-center text-center overflow-hidden z-0'>
-        <HeroBackground backgroundType='About'/>
+    <div className='relative h-screen flex flex-col space-y-8 justify-center items-center text-center overflow-hidden'>
+        <HeroBackground backgroundType="About" />
     </div>
   )
 }
