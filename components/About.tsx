bfe98af8e30caaadcdcee18cf15a94e4df089dfc
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-02-02 20:36:11 +0100

    added react hooks to catch scroll to start about animation

--- components/About.tsx
+++ components/About.tsx
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Animatedwords from "./Animatedwords";
 import HeroBackground from "./HeroBackground";
 
-type Props = {};
+const myString: string = 'As a highly skilled and motivated .NET developer, I am passionate about creating efficient web applications. Continuously seeking opportunities to learn and expand my knowledge on new development technologies, I am able to deliver innovative solutions to complex problems. With a strong team player mentality, I excel in collaborating with others to achieve common goals. My ability to actively listen and understand client requirements has allowed me to effectively meet their needs. I am currently expanding my skills in frontend development with technologies such as React, TypeScript and Tailwind CSS, and eager to take on more responsibility and contribute to the success of both new and existing projects.';
 
-export default function About({}: Props) {
+
+export default function About() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("Element is now visible");
+          setIsVisible(true);
+        } else {
+          console.log("Element is now hidden");
+          setIsVisible(false);
+        }
+      });
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+
   return (
-    <div className="relative h-screen flex flex-col space-y-8 justify-center items-center text-center overflow-hidden m-2 ">
+    <div ref={ref} className="relative h-screen flex flex-col space-y-8 justify-center items-center text-center overflow-hidden m-2 ">
+
       <div className="relative z-0 m-2 h-auto rounded-lg bg-black grid grid-cols-1 grid-rows-1 items-center justify-center  opacity-75 max-w-5xl">
         <div className="h-9 w-full bg-gray-500 opacity-50 rounded-lg"></div>
-        <div className="absolute w-full top-0 z-10 ">
+        <div className="absolute w-full h-auto top-0 z-10 ">
           <p className="m-1 pl-1 text-xl font-semibold md:text-2xl lg:text-2xl font-RobotoSlab text-white text-center">
             About
           </p>
         </div>
-        <div className="m-2 p-4">
-          <p className="text-sm md:text-sm italic lg:text-lg text-left text-white leading-relaxed font-Spline">
-            As a highly skilled and motivated .NET developer, I am passionate
-            about creating efficient web applications. Continuously seeking
-            opportunities to learn and expand my knowledge on new development
-            technologies, I am able to deliver innovative solutions to complex
-            problems.
-            <br />
-            <br />
-            With a strong team player mentality, I excel in collaborating with
-            others to achieve common goals. My ability to actively listen and
-            understand client requirements has allowed me to effectively meet
-            their needs.
-            <br />
-            <br />I am currently expanding my skills in frontend development
-            with technologies such as React, TypeScript and Tailwind CSS, and eager to take
-            on more responsibility and contribute to the success of both new and
-            existing projects.
-          </p>
+        <div className="m-2 p-4 ">
+        {isVisible && (
+            <div className="text-sm md:text-sm italic lg:text-lg text-left text-white leading-relaxed font-Spline">
+              <Animatedwords text={myString} />
+            </div>
+          )}
         </div>
       </div>
+      
     </div>
   );
 }
