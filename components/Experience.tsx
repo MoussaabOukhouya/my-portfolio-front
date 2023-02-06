7c788ce79ba92d21bba705f978844cd04ca342ce
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-02-06 20:01:41 +0100

    added framer motion animation with info update

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 import ExperienceCard from "./ExperienceCard";
 
@@ -5,15 +6,23 @@ type Props = {};
 
 export default function Experience({}: Props) {
   return (
-    <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center z-10">
-      <h3 className="absolute top-24 uppercase tracking-wider text-white text-2xl">
+    <motion.div
+    initial={{opacity:0}}
+    whileInView = {{opacity:1}}
+    transition = {{duration:1.5}}
+    className="h-screen flex relative  overflow-hidden flex-col text-left md:flex-row 
+     px-6 justify-evenly mx-auto items-center max-w-5xl align-text ">
+      <h3 className="absolute top-24 uppercase tracking-[12px] text-white text-2xl ">
         Experience
       </h3>
-
-      <div>
+   
+    <div className=" absolute mt-12 left-0 right-0  w-full flex space-x-5 overflow-x-scroll p-5 snap-x snap-mandatory snap-center  scrollbar scrollbar-track-slate-900 scrollbar-thumb-slate-800 ">
         <ExperienceCard/>
         <ExperienceCard/>
+       
       </div>
-    </div>
+
+     
+    </motion.div>
   );
 }
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,29 +1,52 @@
 import React from 'react'
 import Image from "next/image";
+import cappgemini from "./../assets/Images/capgemini.png";
+import csharp from "./../assets/Images/Techs/c-sharp.png";
+import dotnet from "./../assets/Images/Techs/dotnet.png";
+import visualStudio from "./../assets/Images/Techs/visual-studio.png";
+import svn from "./../assets/Images/Techs/TortoiseSVN.png";
 
-import coding from "./../assets/Images/coding.png";
+import { motion } from 'framer-motion';
 
 type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article>
-        <Image src={coding} alt='coding' className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center "   />
+    <article className='w-3/4 flex flex-col rounded-lg items-center flex-shrink-0
+      snap-center bg-white pb-5 m-2
+     cursor-pointer transition-opacity duration-200 overflow-hidden opacity-40 hover:opacity-100   focus:bg-white '>
+        <motion.div
+        initial = {{
+            y: -100,
+            opacity: 0,
+        }}
+        transition = {{ duration: 1.2}}
+        whileInView = {{opacity:1,y:0}}
+        viewport={{once:true}}>
+        <Image src={cappgemini} alt='coding' className="w-32 h-32 rounded-full object-cover object-center" />
+        </motion.div>
     <div className='px-0 md:px-10'>
-        <h4 className='text-4xl font-light'>.Net developer</h4>
+        <h4 className='text-3xl font-light'>.Net developer</h4>
         <p className='font-bold text-2xl mt-1'>Capgemini</p>
-        <div className='flex space-x-2 my-2'>
+        <div className='flex space-x-2'>
             {/* tech used*/}
+            <Image className='h-10 w-10 rounded-full mt-1' src={csharp} alt='c-sharp' />
+            <Image className='h-10 w-10 rounded-full mt-1' src={dotnet} alt='DotNet' />
+            <Image className='h-10 w-10 rounded-full mt-1' src={visualStudio} alt='Visual Studio' />
+            <Image className='h-10 w-10 rounded-full mt-1' src={svn} alt='Tortoise SVN' />
+
         </div>
     </div>
-    <p>date</p>
-    <ul>
-        <li>Summary Points</li>
-        <li>Summary Points</li>
-        <li>Summary Points</li>
-        <li>Summary Points</li>
-        <li>Summary Points</li>
+    <div className='p-4'>
+    <p className='uppercase text-center mb-2 italic'>October 25 2021 - Present</p>
+    <ul className='list-disc space-y-1 ml-5 text-base'>
+        <li>Development of the different user stories.</li>
+        <li>Maintenance of the various project applications and correction of anomalies</li>
+        <li>Implementation of SQL database scripts.</li>
+        <li> Preparation of delivery packages.</li>
     </ul>
+    </div>
+   
     </article>
   );
 }
